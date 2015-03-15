ebc122267c43d4ed6c8b3291a5955785602294d3
probonopd <probono@puredarwin.org>
2015-03-15 18:57:11 +0100

    Show episodes immediately when opening the plugin

--- default.py
+++ default.py
@@ -6,24 +6,8 @@ __date__ = '20-06-2014'
 __version__ = '0.2.6'
 __settings__ = xbmcaddon.Addon(id='plugin.video.eevblog')
 
-# Thanks to some of the other plugin authors, where I borrowed some ideas from !
 EEV_URL='http://www.eevblog.com'
 
-REMOTE_DBG = False
-
-# append pydev remote debugger
-if REMOTE_DBG:
-  # Make pydev debugger works for auto reload.
-  # Note pydevd module need to be copied in XBMC\system\python\Lib\pysrc
-  try:
-    import pysrc.pydevd as pydevd
-  # stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
-    pydevd.settrace('localhost', stdoutToServer=True, stderrToServer=True)
-  except ImportError:
-    sys.stderr.write("Error: " +
-      "You must add org.python.pydev.debug.pysrc to your PYTHONPATH.")
-    sys.exit(1)
-    
 def open_url(url):
   req = urllib2.Request(url)
   req.add_header("user-agent","Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)")
@@ -34,23 +18,17 @@ def open_url(url):
 
 def build_main_directory():
   image = "icon.png"
-  listitem = xbmcgui.ListItem(label = "Episode Listing", iconImage = image, thumbnailImage = image)
-  u = sys.argv[0] + "?mode=1"
-  xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = u, listitem = listitem, isFolder = True)
-
-def build_episodes_directory():
   url = EEV_URL + '/episodes/'
   data = open_url(url)
   match = re.compile('<body>(.+?)</body>', re.DOTALL).findall(data)
   youtube_url_name = re.compile(r'<a href.?="(.+?)" title="(EEVblog #.+?)">', re.IGNORECASE).findall(match[0])
   for ep_url, name in youtube_url_name:
     listitem = xbmcgui.ListItem(label = name, iconImage = "", thumbnailImage = "")
-    #listitem.setInfo( type = "Video", infoLabels = { "Title": name, "Director": __plugin__, "Studio": __plugin__, "Genre": "Video Blog", "Plot": plot[0], "Episode": "" } )
     if ep_url:
       u = sys.argv[0] + "?mode=2&url=" + ep_url + "&name=" + name
       xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = u, listitem = listitem, isFolder = False)
       xbmcplugin.addSortMethod( handle = int(sys.argv[ 1 ]), sortMethod = xbmcplugin.SORT_METHOD_NONE )
-    
+
 def clean(name):
   remove = [('&amp;','&'), ('&quot;','"'), ('&#039;','\''), ('\r\n',''), ('&apos;','\''), ('.','')]
   for trash, crap in remove:
